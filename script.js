e5eb070999e5cc84186e27a9041312f56f2757e5
Sarah Carlson <sbcarls@gmail.com>
2020-10-10 21:59:49 -0400

    added some js code and an assets folder

---
+++ script.js
@@ -0,0 +1,25 @@
+// 
+//
+//
+//
+//
+//
+var startBtn = $("#start-button");
+var choices = ["a", "b", "c", "d"];
+var timer = $("#time-clock");
+var 
+var 
+// var timeInterval = ? 
+
+
+(function() {
+    for (var i=, i < ????? , i++)
+});
+
+function setTime() {
+    var  setInterval(function() {
+
+    }
+}
+
+
